76421569de7ceea597c6ea9b55a56aa1151ebbee
Jeff Hardy <jdhardy@gmail.com>
2012-04-01 23:40:45 -0700

    Initial commit of the game project and the console sample.

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IronPython.Hosting;
+
+namespace ConsoleSample {
+    class Program {
+        static void Main(string[] args) {
+            var engine = Python.CreateEngine();
+            engine.ExecuteFile("FlippingGame/FlippingGame.py");
+        }
+    }
+}
