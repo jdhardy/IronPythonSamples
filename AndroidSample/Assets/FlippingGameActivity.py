3d56e71ad76883f4594c7e7dc5ea7ab4970dd988
Jeff Hardy <jdhardy@gmail.com>
2012-06-10 22:42:52 -0700

    Add IronPython sample.

---
+++ AndroidSample/Assets/FlippingGameActivity.py
@@ -0,0 +1,22 @@
+﻿from AndroidSample import Resource
+
+class FlippingGameActivity(object):
+    def __init__(self, main):
+        self.main = main
+        self.count = 1
+
+    def OnCreate(self, bundle):
+        # Set our view from the "main" layout resource
+        self.main.SetContentView(Resource.Layout.Main);
+
+        self.resultLabel = self.main.FindViewById(Resource.Id.resultLabel)
+        self.resultLabel.Text = str(self.count)
+
+        # Get our button from the layout resource,
+        # and attach an event to it
+        self.flipButton = self.main.FindViewById(Resource.Id.flipButton)
+        self.flipButton.Click += lambda sender, e: self.button_Click(sender, e)
+
+    def button_Click(self, sender, e):
+        self.count += 1
+        self.resultLabel.Text = str(self.count)
---
+++ AndroidSample/FlippingGameActivity.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.Scripting.Hosting;
+using IronPython.Hosting;
+using System.IO;
+
+namespace AndroidSample {
+    [Activity(Label = "AndroidSample", MainLauncher = true, Icon = "@drawable/icon")]
+    public class FlippingGameActivity : Activity {
+        int count = 1;
+
+        ScriptEngine engine = Python.CreateEngine();
+        ScriptScope scope = null;
+        object shadow;
+
+        public FlippingGameActivity() {
+            this.scope = this.engine.CreateScope();
+            this.engine.Runtime.LoadAssembly(typeof(Resource).Assembly);
+        }
+
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            InitShadow();
+            this.engine.Operations.InvokeMember(this.shadow, "OnCreate", bundle);
+        }
+
+        private void InitShadow() {
+            string code = null;
+            using(var shadowStream = new StreamReader(this.Assets.Open("FlippingGameActivity.py"))) {
+                code = shadowStream.ReadToEnd();
+            }
+
+            this.engine.Execute(code, this.scope);
+
+            object shadowClass = this.scope.GetVariable("FlippingGameActivity");
+            this.shadow = this.engine.Operations.CreateInstance(shadowClass, this);
+        }
+    }
+}
+
